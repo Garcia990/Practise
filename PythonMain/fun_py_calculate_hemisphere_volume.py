2bdd3d5c82f987784f9b08d2736694129ebb1636
Garcia990 <garcia_isabella_98@163.com>
2024-07-23 22:26:52 +0800

    Create fun_py_calculate_hemisphere_volume.py

---
+++ PythonMain/fun_py_calculate_hemisphere_volume.py
@@ -0,0 +1,7 @@
+# fun_py_calculate_hemisphere_volume.py
+import math
+
+def fun_py_calculate_hemisphere_volume(radius):
+    return (2/3) * math.pi * radius ** 3
+
+print(fun_py_calculate_hemisphere_volume(3))
