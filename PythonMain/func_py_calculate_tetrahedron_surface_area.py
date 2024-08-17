2019fdd83cd51f8dd501d2cefc6fd6a24624b7ba
Garcia990 <garcia_isabella_98@163.com>
2024-08-17 19:23:08 +0800

    Create func_py_calculate_tetrahedron_surface_area.py

---
+++ PythonMain/func_py_calculate_tetrahedron_surface_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_tetrahedron_surface_area.py
+import math
+
+def func_py_calculate_tetrahedron_surface_area(edge_length):
+    return math.sqrt(3) * edge_length**2
+
+print(func_py_calculate_tetrahedron_surface_area(4))
