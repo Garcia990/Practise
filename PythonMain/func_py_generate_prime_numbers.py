bf0b613f1ca78c030964f41fe792ceebe0c7f129
Garcia990 <garcia_isabella_98@163.com>
2024-09-21 11:11:30 +0800

    Create func_py_generate_prime_numbers.py

---
+++ PythonMain/func_py_generate_prime_numbers.py
@@ -0,0 +1,9 @@
+# func_py_generate_prime_numbers.py
+def func_py_generate_prime_numbers(limit):
+    primes = []
+    for num in range(2, limit+1):
+        if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)):
+            primes.append(num)
+    return primes
+
+print(func_py_generate_prime_numbers(20))
