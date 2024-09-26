302f93f670d1ee41d2ffe75a5da37ae9fbef8483
Garcia990 <garcia_isabella_98@163.com>
2024-09-26 17:22:53 +0800

    Create func_py_generate_multiplication_tables.py

---
+++ PythonMain/func_py_generate_multiplication_tables.py
@@ -0,0 +1,5 @@
+# func_py_generate_multiplication_tables.py
+def func_py_generate_multiplication_table(n):
+    return [n * i for i in range(1, 11)]
+
+print(func_py_generate_multiplication_table(5))
